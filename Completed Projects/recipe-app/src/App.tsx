6ee4b05309d647be0c05231ca05689e59f803a47
Michelle <shellayywow@gmail.com>
2021-10-11 23:33:33 -0500

    Changed up styling. Added in input box so recipes can be added by the user.

--- Completed Projects/recipe-app/src/App.tsx
+++ Completed Projects/recipe-app/src/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import List from './components/List'
+import AddToList from './components/AddToList'
 import './App.css';
 
 export interface IState {
-  recipes: [
+  recipes: 
     {
       name: string
-      ingredients: Array<string>
+      ingredients: string[]
       instructions: string
-    }
-  ]
+    }[]
 }
 
 function App() {
@@ -20,21 +20,21 @@ function App() {
       name: "Koski Chicken",
       ingredients: ["1 can cream of mushroom soup", "1 packet dry onion soup mix", "1 C water", "2 C rice", "1 lb. chicken legs"],
       instructions: "Mix together soup, dry soup packet, and water. Put liquid mixture in a baking dish and mix in the rice. Add chicken on top of the rice and bake at 350 degrees farenheit for 1 1/2 hours. Remove from oven, flip over chicken, and return to oven for 1 more hour or so. Let sit about 10 minutes."
+    },
+    {
+      name: "Lemon Pepper Broccoli",
+      ingredients: ["Lemon pepper seasoning", "Olive oil", "Broccoli"],
+      instructions: "Mix together all ingredients."
     }
-      // },
-    // {
-    //   name: "Lemon Pepper Broccoli",
-    //   ingredients: ["Lemon pepper seasoning", "Olive oil", "Broccoli"],
-    //   instructions: "Mix together all ingredients."
-    // }
   ])
 
   return (
     <div className="content">
       <div className="App-header">
-      Recipe App
+      Recipe Keeper
       </div>
       <List recipes={recipes}/>
+      <AddToList setRecipes={setRecipes} recipes={recipes}/>
     </div>
   );
 }
---
+++ Completed Projects/recipe-app/src/components/AddToList.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { IState as Props } from "../App"
+
+interface IProps {
+    setRecipes: React.Dispatch<React.SetStateAction<Props["recipes"]>>
+    recipes: Props["recipes"]
+}
+
+const AddToList: React.FC<IProps> = ({setRecipes, recipes}) => {
+    const [input, setInput] = useState({
+        name: "",
+        ingredients: [],
+        instructions: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = () => {
+        if(!input.name || !input.ingredients || !input.instructions) return
+
+        setRecipes([
+            ...recipes,
+            {
+                name: input.name,
+                ingredients: input.ingredients,
+                instructions: input.instructions
+            }
+        ]);
+
+        setInput({
+            name: "",
+            ingredients: [],
+            instructions: ""
+        })
+    }
+
+    return (
+        <div className="AddToList">
+            <h2>Add Recipe to List</h2>
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="name"
+                value={input.name}
+                placeholder="Recipe Name"
+            />
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="ingredients"
+                value={input.ingredients}
+                placeholder="Ingredients separated by commas"
+            />
+            <textarea
+                onChange={handleChange}
+                className="AddToList-input"
+                name="instructions"
+                value={input.instructions}
+                placeholder="Instructions"
+            />
+            <button
+                onClick={handleClick}
+                className="AddToList-btn"
+            >
+                Add to List
+            </button>
+        </div>
+    )
+}
+
+export default AddToList

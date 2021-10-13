9ac4628dbbfcf2448fcb4d0f2e664a2d619fa1ca
MichelleCode <shellayywow@gmail.com>
2021-10-12 20:42:27 -0500

    Preset recipe ingredients are being iterated through. They are appearing as raw HTML, figuring out how to convert the HTML to JSX to fix.

--- Completed Projects/recipe-app/src/components/List.tsx
+++ Completed Projects/recipe-app/src/components/List.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { IState as Props } from "../App"
+import React from 'react';
+import { IState as Props } from "../App";
+//import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
 
 interface IProps {
     recipes: Props["recipes"]
@@ -8,6 +9,15 @@ interface IProps {
 const List: React.FC<IProps> = ({ recipes }) => {
     
     const renderList = (): JSX.Element[] => {
+        var str = '<ul>'
+        recipes.forEach(function(recipe) {
+            recipe.ingredients.forEach(function(ingredient) {
+                str += '<li>' + ingredient + '</li>';
+            });
+        });
+
+        str += '</ul>';
+
         return recipes.map(recipe => {
             return(
                 <li className="List">
@@ -15,7 +25,7 @@ const List: React.FC<IProps> = ({ recipes }) => {
                         <h2>{recipe.name}</h2>
                     </div>
                     <div className="List-ingredients">
-                        <p>{recipe.ingredients}</p>
+                        <p>{str}</p>
                     </div>
                     <div className="List-instructions">
                         <p>{recipe.instructions}</p>

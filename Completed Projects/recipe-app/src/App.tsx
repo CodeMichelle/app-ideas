a906e9410a0d26418fbc33dd25381bd9cda6ccbc
MichelleCode <shellayywow@gmail.com>
2021-10-12 22:19:55 -0500

    Added some more recipe info.

--- Completed Projects/recipe-app/src/App.tsx
+++ Completed Projects/recipe-app/src/App.tsx
@@ -19,12 +19,12 @@ function App() {
     {
       name: "Koski Chicken",
       ingredients: "1 can cream of mushroom soup, 1 packet dry onion soup mix, 1 C water, 2 C rice, 1 lb. chicken legs",
-      instructions: "Mix together soup, dry soup packet, and water. Put liquid mixture in a baking dish and mix in the rice. Add chicken on top of the rice and bake at 350 degrees farenheit for 1 1/2 hours. Remove from oven, flip over chicken, and return to oven for 1 more hour or so. Let sit about 10 minutes."
+      instructions: "Preheat oven to 350 degrees F. Mix together soup, dry soup packet, and water. Put liquid mixture in a baking dish and mix in the rice. Add chicken on top of the rice and bake for 1 1/2 hours. Remove from oven, flip over chicken, and return to oven for 1 more hour or until the rice is cooked. Let sit about 10 minutes."
     },
     {
-      name: "Lemon Pepper Broccoli",
-      ingredients: "Lemon pepper seasoning, Olive oil, Broccoli",
-      instructions: "Mix together all ingredients."
+      name: "Oven Roasted Broccoli",
+      ingredients: "Lemon pepper seasoning, Olive oil, Broccoli, Salt and Pepper",
+      instructions: "Preheat oven to 400 degrees F. Cut broccoli into bite size pieces and toss with olive oil. Add broccoli to a greased baking sheet and add salt and pepper to taste. Bake broccoli for 15 minutes, remove from oven and toss, then bake in the oven for another 15 minutes."
     }
   ])
 
